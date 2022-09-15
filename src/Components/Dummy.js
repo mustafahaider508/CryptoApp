fb71add9a31bf5ceef72b83e90f55f2b75293302
Mustafa Haider <42119893+mustafahaider508@users.noreply.github.com>
2022-09-15 02:05:05 -0700

    updated

--- src/Components/Dummy.js
+++ src/Components/Dummy.js
@@ -2,44 +2,53 @@ import React,{useEffect,useState} from 'react';
 import axios from "axios";
 function Dummy() {
 
-   const[apidata,setApidata] = useState([]);
+
+
+
+   const[apidata,setApidata] = useState([{name:""}]);
     const [coin,setCoin] = useState([]);
+
+
+
+    var PostData = async () => {
+ 
+      
+
+console.log('Client',coin[0]);
+
+
+axios.post("/coins",coin[0])
+.then((res)=>{console.log(res.data)}).then((err)=>{("error "+err)})
+
+alert("Product Added Successfully");
+
+
+
+      
+
+    
+    }
 
 
     useEffect(() => {
          axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-         .then((res)=>{setCoin(res.data.filter(x => (x.symbol === "eth") ||(x.symbol === "bnb") || (x.symbol === "matic")).map(i => {return  i.last_updated}))});
+         .then((res)=>{setCoin(res.data.filter(x => (x.symbol === "eth") ||(x.symbol === "bnb") || (x.symbol === "matic")).map(i => {return i.name}))});
     
- console.log(coin)
 
+    setApidata({name:coin[0]});
         },[])
-
-// useEffect(() => {
-//   const data =  coin.map(i => {return i.name })
-//   setApidata(data);
+        console.log(apidata)
 
 
-// },[])
   
-// console.log(apidata);
+console.log(apidata);
 
 
 
   return (
     <>
   
-    {coin.map(i => {
-           return(
-               <>
-               <p>{i.name}</p>
-               <p>{i.last_updated}</p>
-               <p>{i.current_price}</p>
-          
-
-               </>
-           )
-       })
-      }
+    <button onClick={PostData}>click Me</button>
       
     </>
   )
