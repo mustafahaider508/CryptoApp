8c531d0e2a5e9e94694feb9c24700d52ed3eaed8
Mustafa Haider <42119893+mustafahaider508@users.noreply.github.com>
2022-09-15 05:31:17 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import CoinsTable from './Components/CoinsTable';
 import Homepage  from './Components/Homepage';
 import Graph from "./Components/Graph";
 import  AppexChart from "./Components/Chart";
-import Dummy from './Components/Dummy';
+import Dummy from './Components/ApiData';
+import ApiData from './Components/ApiData';
 
 
 function App() {
   return (
     <div className="App">
-      <Dummy />
+    
 
-      {/* <BrowserRouter>
+       <BrowserRouter>
       <Routes>
         <Route  path="/" element={<Homepage />} />
         <Route path="/coinstable"   element={<CoinsTable />} />
         <Route  path="/graph/:id" element={ <Graph/>}  />
+        <Route  path="/apidata" element={<ApiData />}/>
       </Routes>
-      </BrowserRouter>  */}
+      </BrowserRouter>  
 
-   {/* <ApexChart /> */}
-
-      
-
-     {/* <Homepage /> */}
-     {/* <CoinsTable /> */}
+ 
     </div>
   );
 }
